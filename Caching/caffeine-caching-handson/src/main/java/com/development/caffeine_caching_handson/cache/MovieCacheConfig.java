9264b4d2e6b361e1d5d51882d57679de18d33d80
Yash Vardhan Mishra <yashvardhanmishra01@gmail.com>
2026-06-25 16:28:32 +0530

    Automated and Manual Caching Mechanism

--- Caching/caffeine-caching-handson/src/main/java/com/development/caffeine_caching_handson/cache/MovieCacheConfig.java
+++ Caching/caffeine-caching-handson/src/main/java/com/development/caffeine_caching_handson/cache/MovieCacheConfig.java
@@ -1,15 +1,29 @@
 package com.development.caffeine_caching_handson.cache;
 
+import com.development.caffeine_caching_handson.repository.DataDao;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class MovieCacheConfig {
 
+	private final DataDao dataDao;
+
+	// for manual caching purpose
 	@Bean
-	public Cache<String, String> movieCache() {
+	public Cache<String, String> manualMovieCache() {
 		return Caffeine.newBuilder().build();
+	}
+
+	// for automated caching purpose
+	@Bean
+	public LoadingCache<String, String> automatedMovieCache() {
+		return Caffeine.newBuilder().build(dataDao::movieByDirector);
 	}
 }
--- Caching/caffeine-caching-handson/src/main/java/com/development/caffeine_caching_handson/controller/MovieController.java
+++ Caching/caffeine-caching-handson/src/main/java/com/development/caffeine_caching_handson/controller/MovieController.java
@@ -2,9 +2,11 @@ package com.development.caffeine_caching_handson.controller;
 
 import com.development.caffeine_caching_handson.repository.DataDao;
 import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,10 +14,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
 	private final DataDao dataDao;
-	private final Cache<String, String> movieCache;
+
+	// used for manually populating the cache
+	private final Cache<String, String> manualMovieCache;
+
+	// used for automated populating the cache
+	private final LoadingCache<String, String> automatedMovieCache;
 
 	@GetMapping("/movie/{director}")
-	public String getMovieByDirector(@PathVariable String director) {
-		return movieCache.get(director, dataDao::movieByDirector);
+	public String getMovieByDirectorManualCache(@PathVariable String director) {
+
+		// manually populating the cache, when we ourselves gives the logic on
+		// how to populate the cache when it misses, then it is Manual Population
+		// of cache.
+		return manualMovieCache.get(director, dataDao::movieByDirector);
+	}
+
+	@GetMapping("/movie")
+	public String getMovieByDirectorAutomatedCache(@RequestParam String director) {
+
+		// automated populating the cache, we just pass the key value, and
+		// if misses then the cache automatically handles the miss and populates
+		// the cache itself.
+		return automatedMovieCache.get(director);
 	}
 }
